6db4268dcb4f2a8a45c08d5da9956cec1557a2c7
Matthew Lim Desktop X300 <matthew_lim_bh@hotmail.com>
2026-03-26 10:19:07 +0800

    deployed wrong place-- cleanup

--- assets/buildInfo-Bs3nmV4-.js
+++
@@ -1,1 +0,0 @@
-const s=await fetch("./build-info.json"),{gitHash:t,buildTime:a,gitTime:i}=await s.json(),o=t,n=a,c=i;export{n as B,o as G,c as a};
---
+++ readme.md
@@ -0,0 +1,4 @@
+# claude-cv-windows95-dist
+webpage
+
+This is the deployed page of cv.lbh.rocks
